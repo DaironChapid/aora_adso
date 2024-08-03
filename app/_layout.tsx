57675cc9ccc960d6485992cfc60c4f2fefc25afe
DaironChapid <dairon_chapid@soy.sena.edu.co>
2024-08-03 18:39:40 -0500

    Video 3 Terminado

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,7 +6,8 @@ import { Slot, Stack } from "expo-router"
 const  RootLayout = () => {
 return (
   <Stack>
-    <Stack.Screen name="index" options={{ headerShown: false }} />
+    <Stack.Screen name="index" 
+    options={{ headerShown: false }} />
   </Stack>
 )
 }
